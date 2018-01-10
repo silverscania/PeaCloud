e82d74b123473ae4900cda5c6dabb7071050622b
James Clark <silverscania@gmail.com>
2018-01-10 19:59:19 +0000

    add disk by UUID and un-revisioned settings

--- settings.sh
+++ settings.sh
@@ -1,9 +1,17 @@
-export PASSPHRASE="my gpg password"
+# mysettings.sh should contain the following filled in and
+# uncommented
 
-export AWS_ACCESS_KEY_ID="aws access key"
-export AWS_SECRET_ACCESS_KEY="aws secret key"
-export AWS_DATA_BUCKET="s3://s3-ap-south-1.amazonaws.com/my-bucket"
-export AWS_DB_BUCKET="s3://s3-ap-south-1.amazonaws.com/my-db-bucket"
-export EMAIL_RECIPIENTS="email@one.com,email@two.com"
-export UPLOAD_TIMEOUT="8h"
+#export PASSPHRASE="my gpg password"
+
+#export AWS_ACCESS_KEY_ID="aws access key"
+#export AWS_SECRET_ACCESS_KEY="aws secret key"
+#export AWS_DATA_BUCKET="s3://s3-ap-south-1.amazonaws.com/my-bucket"
+#export AWS_DB_BUCKET="s3://s3-ap-south-1.amazonaws.com/my-db-bucket"
+#export EMAIL_RECIPIENTS="email@one.com,email@two.com"
+#export UPLOAD_TIMEOUT="8h"
+
+. ./mysettings.sh
+
+
+
 
--- unencryptDisk.sh
+++ unencryptDisk.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo cryptsetup luksOpen /dev/sdc5 nextcloud
+sudo cryptsetup luksOpen /dev/disk/by-uuid/71a2e23e-90dd-4ad6-86d3-2cda6926bc1c nextcloud
 sudo mount /dev/mapper/nextcloud /mnt/nextcloud_encrypted/
